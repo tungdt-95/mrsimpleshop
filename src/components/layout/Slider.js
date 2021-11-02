fb6adb8326337389fb1332e996a3b47f52e439ff
tungdt-95 <doantrantung@gmail.com>
2021-11-02 15:03:55 +0700

    update slider

--- src/components/layout/Slider.js
+++ src/components/layout/Slider.js
@@ -2,7 +2,27 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
+function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "rgb(95, 158, 160)" }}
+        onClick={onClick}
+      />
+    );
+  }
+  
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "rgb(95, 158, 160)"}}
+        onClick={onClick}
+      />
+    );
+  }
 function SliderList (){
     let settings = {
         dots: true,
@@ -10,12 +30,14 @@ function SliderList (){
         speed: 1000,
         slidesToShow: 1,
         slidesToScroll: 1,
-        cssEase:"linear"
+        cssEase:"linear" ,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />    
       };
     return(
-        <div className="grid main">
+        <div className="grid wide">
             <div className="row">
-                <div className="col l-12">
+                <div className="col l-12 slider">
                     <Slider {...settings}>
                         <div>
                             <img alt="ha" src="https://mrsimple.vn/images/slider/desktop1628852808.jpg" className="slider_1"></img>
